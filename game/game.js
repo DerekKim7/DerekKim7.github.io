dce9302f71ce2955f165fd91adfb4122f71f40ae
DerekKim7 <dancingdboys@gmail.com>
2015-11-05 14:36:47 +0800

    Day 4

--- game/game.js
+++ game/game.js
@@ -4,8 +4,8 @@ $("#start").click(function(){
 	$("#balls").css("background-image", "url('http://images6.alphacoders.com/310/310337.jpg')");
 $("#start").hide();
 $("#b-bounce").hide();
-
 	}); 
+$("#platform").show(); 
 
 }); 
 
